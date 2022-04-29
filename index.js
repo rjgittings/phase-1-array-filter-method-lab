98378437a3e380d50e762a9272463832ff5eaf5d
Robert Gittings <rjgitting@gmail.com>
2022-04-29 13:36:10 -0400

    commit tree 1b61279f8e3d4857448af4afcab9efe4422b6a0c

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];  
+
+function findMatching(array, string){
+  return array.filter(
+    (possibleMatch => possibleMatch.toLowerCase() === string.toLowerCase())
+  );
+  }
+
+function fuzzyMatch (source, letter){
+return source.filter (driver => (driver.startsWith(letter)
+));
+}
+
+
+function matchName (source, inputString){
+  return source.filter(record => record.name === inputString);
+  }
